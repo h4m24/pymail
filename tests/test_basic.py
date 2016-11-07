5793eb35230e6f66ab8d30f3fd9acaad05baf701
hamza <hamza.faouri@zipjet.com>
2016-11-07 09:24:14 +0100

    added a test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,7 @@
 import unittest
+from pymailer.helpers import *
 
 
 class MyTest(unittest.TestCase):
     def test(self):
-        self.assertEqual(list_to_string(['a', 'b', 'c']), 'abc')
+        self.assertEqual(type(get_event_data('{"test_key" : "test_value" }')), dict)

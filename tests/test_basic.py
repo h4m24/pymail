e2120268bad59d9b4936389ac98f6269e646eb89
hamza <hamza.faouri@zipjet.com>
2016-11-07 09:48:48 +0100

    added tests
    added json test file

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,5 +3,13 @@ from pymailer.helpers import *
 
 
 class MyTest(unittest.TestCase):
-    def test(self):
+
+    def test_get_event_returns_dict(self):
         self.assertEqual(type(get_event_data('{"test_key" : "test_value" }')), dict)
+
+    def test_read_alerter_conf_file(self):
+        self.assertEqual(type(read_alerter_conf_file('./test.json')), dict)
+
+
+if __name__ == '__main__':
+    unittest.main()
